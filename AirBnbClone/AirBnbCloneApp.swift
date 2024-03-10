a37cdbe6fdafb392a6cb904fe83aaab8a6919661
DucTuan <tuanhypc02@gmail.com>
2024-03-10 14:55:34 +0700

    Initial Commit

---
+++ AirBnbClone/AirBnbCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirBnbCloneApp.swift
+//  AirBnbClone
+//
+//  Created by macOS on 10/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirBnbCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
